28757424455585900fd904b14e575c41c3bae95d
nechaudhary <nechaudhary@paypal.com>
2022-05-15 15:31:03 +0530

    h2-console accessing & interface to accept all entities

--- src/main/java/com/RestfulWebServices/jpa/jpafirst/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/RestfulWebServices/jpa/jpafirst/UserRepositoryCommandLineRunner.java
@@ -2,36 +2,32 @@ package com.RestfulWebServices.jpa.jpafirst;
 
 import com.RestfulWebServices.jpa.jpafirst.entity.User;
 import com.RestfulWebServices.jpa.jpafirst.service.UserDAOService;
+import com.RestfulWebServices.jpa.jpafirst.service.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import lombok.extern.slf4j.XSlf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import javax.activation.CommandObject;
-import javax.transaction.Transactional;
 
 @Component
 @Order(1)
 // alternative for Logger log
 @Slf4j
-public class UserDaoServiceCommandLineRunner implements CommandLineRunner {
+public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
     // private static final Logger log = LoggerFactory.getLogger(UserDaoServiceCommandLineRunner.class);
     @Autowired
-    private UserDAOService userDAOService;
+    private UserRepository userRepository;
 
     @Override
     public void run(String... args) throws Exception {
         User user = new User();
-        user.setName("Neha");
+        user.setName("Alison Wonderland");
         user.setRole("Admin");
-        long insert = userDAOService.insert(user);
+
+        // will return a user
+        userRepository.save(user);
+
         log.info("New User created: "+ user);
 
     }
